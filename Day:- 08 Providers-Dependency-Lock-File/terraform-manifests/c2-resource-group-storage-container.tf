adae29966d5be1e3fc3167e9ff4edc4a498812b3
Ankit Ranjan <154872782+ankitnewjobs@users.noreply.github.com>
2024-11-09 13:43:15 +0530

    Update c2-resource-group-storage-container.tf

--- Day:- 08 Providers-Dependency-Lock-File/terraform-manifests/c2-resource-group-storage-container.tf
+++ Day:- 08 Providers-Dependency-Lock-File/terraform-manifests/c2-resource-group-storage-container.tf
@@ -1,20 +1,23 @@
 # Resource-1: Azure Resource Group
 
-resource "azurerm_resource_group" "myrg1" {
+resource "azurerm_resource_group" "myrg1" 
+{
   name = "myrg-1"
   location = "East US"
 }
 
 # Resource-2: Random String 
 
-resource "random_string" "myrandom" {
+resource "random_string" "myrandom" 
+{
   length = 16
   upper = false 
   special = false
 }
 
 # Resource-3: Azure Storage Account 
-resource "azurerm_storage_account" "mysa" {
+resource "azurerm_storage_account" "mysa" 
+{
   name                     = "mysa${random_string.myrandom.id}"
   resource_group_name      = azurerm_resource_group.myrg1.name
   location                 = azurerm_resource_group.myrg1.location
