d09db7cac877a366be1c89c853e13fff29e390f6
Ankit Ranjan <154872782+ankitnewjobs@users.noreply.github.com>
2024-11-09 13:32:55 +0530

    Update top-level-blocks-samples.tf

--- Day:- 04 Terraform-Language-Syntax/terraform-manifests/top-level-blocks-samples.tf
+++ Day:- 04 Terraform-Language-Syntax/terraform-manifests/top-level-blocks-samples.tf
@@ -6,7 +6,8 @@ terraform {
     azurerm = {
       source = "hashicorp/azurerm"
       version = ">= 2.0"
-    }    }
+    }    
+}
 
 # Terraform State Storage to Azure Storage Container
 
@@ -15,7 +16,8 @@ terraform {
     storage_account_name  = "terraformstate201"
     container_name        = "tfstatefiles"
     key                   = "terraform.tfstate"
-  }    }
+  }   
+}
 
 ------------
 
