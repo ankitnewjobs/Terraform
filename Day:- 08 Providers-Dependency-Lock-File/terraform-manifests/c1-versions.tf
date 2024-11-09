5a66baa9230b3c7620fac08ecf426c15fa7144ff
Ankit Ranjan <154872782+ankitnewjobs@users.noreply.github.com>
2024-11-09 13:42:43 +0530

    Update c1-versions.tf

--- Day:- 08 Providers-Dependency-Lock-File/terraform-manifests/c1-versions.tf
+++ Day:- 08 Providers-Dependency-Lock-File/terraform-manifests/c1-versions.tf
@@ -1,21 +1,28 @@
 # Terraform Block
 
-terraform {
+terraform 
+{
   required_version = ">= 0.15"
-  required_providers {
-    azurerm = {
+  required_providers 
+{
+    azurerm = 
+{
       source = "hashicorp/azurerm"
       version = "1.44.0"
       #version = ">= 2.0" 
     }
-    random = {
+    random = 
+{
       source = "hashicorp/random"
       version = ">= 3.0"
-    }   }  }
+    }  
+}
+}
 
 # Provider Block
 
-provider "azurerm" {
+provider "azurerm" 
+{
 # features {}          
 }
 
