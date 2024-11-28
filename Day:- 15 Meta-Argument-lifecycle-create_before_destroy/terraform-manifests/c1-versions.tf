9f473dfbaf9956d3a2a98b8a4cc7711a4cec1bca
Ankit Ranjan <154872782+ankitnewjobs@users.noreply.github.com>
2024-11-28 08:22:57 +0530

    Update c1-versions.tf

--- Day:- 15 Meta-Argument-lifecycle-create_before_destroy/terraform-manifests/c1-versions.tf
+++ Day:- 15 Meta-Argument-lifecycle-create_before_destroy/terraform-manifests/c1-versions.tf
@@ -17,6 +17,7 @@ provider "azurerm" {
 
 ------------------------------------------------------------------------------------------------------------------------
 
+# Explanation: - 
 The provided Terraform code defines the Terraform block and the Provider block, both of which are essential for configuring a Terraform project to work with the Azure platform. Let’s break it down in detail:
 
 ### Terraform Block
