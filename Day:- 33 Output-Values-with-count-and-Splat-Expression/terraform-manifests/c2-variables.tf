54ab541fa3382fee12b2e1d42bebdd0b04ce7a75
Ankit Ranjan <154872782+ankitnewjobs@users.noreply.github.com>
2025-02-18 09:26:02 +0530

    Update c2-variables.tf

--- Day:- 33 Output-Values-with-count-and-Splat-Expression/terraform-manifests/c2-variables.tf
+++ Day:- 33 Output-Values-with-count-and-Splat-Expression/terraform-manifests/c2-variables.tf
@@ -1,24 +1,28 @@
 # Input Variables
 
 # 1. Business Unit Name
+
 variable "business_unit" {
   description = "Business Unit Name"
   type = string
   default = "hr"
 }
 # 2. Environment Name
+
 variable "environment" {
   description = "Environment Name"
   type = string
   default = "poc"
 }
 # 3. Resource Group Name
+
 variable "resoure_group_name" {
   description = "Resource Group Name"
   type = string
   default = "myrg"
 }
 # 4. Resource Group Location
+
 variable "resoure_group_location" {
   description = "Resource Group Location"
   type = string
@@ -26,6 +30,7 @@ variable "resoure_group_location" {
 }
 
 # 5. Virtual Network Name
+
 variable "virtual_network_name" {
   description = "Virtual Network Name"
   type = string 
