bcdc84c1d1b113403208ca7203ce90503c2ce825
Ankit Ranjan <154872782+ankitnewjobs@users.noreply.github.com>
2025-03-01 08:16:12 +0530

    Update c3-local-values.tf

--- Day:- 35 Terraform-Local-Values/terraform-manifests/c3-local-values.tf
+++ Day:- 35 Terraform-Local-Values/terraform-manifests/c3-local-values.tf
@@ -1,11 +1,14 @@
 # Local Values Block
 
 locals {
+
   # Use-case-1: Shorten the names for more readability
+
   rg_name = "${var.business_unit}-${var.environment}-${var.resoure_group_name}"
   vnet_name = "${var.business_unit}-${var.environment}-${var.virtual_network_name}"
 
   # Use-case-2: Common tags to be assigned to all resources
+
   service_name = "Demo Services"
   owner = "Kalyan Reddy Daida"
   common_tags = {
@@ -14,6 +17,7 @@ locals {
   }
 
   # Use-case-3: Terraform Dynamic or Conditional Expressions
+
   # We will learn this when we are dealing with Dynamic Expressions
   # The expressions assigned to local value names can either be simple constants or can be more complex expressions that transform or combine values from elsewhere in the module.
  
