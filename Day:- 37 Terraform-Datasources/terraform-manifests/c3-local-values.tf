c43cb2337d2462d5af594d6469375f2a1dfcbb89
Ankit Ranjan <154872782+ankitnewjobs@users.noreply.github.com>
2025-03-17 20:36:58 +0530

    Update c3-local-values.tf

--- Day:- 37 Terraform-Datasources/terraform-manifests/c3-local-values.tf
+++ Day:- 37 Terraform-Datasources/terraform-manifests/c3-local-values.tf
@@ -16,18 +16,23 @@ locals {
     Owner = local.owner
   }
 
-  # Use-case-3: Terraform Dynamic or Conditional Expressions
+  # Use-case-3: Terraform Dynamic Expressions
 
   # We will learn this when we are dealing with Dynamic Expressions
-  # The expressions assigned to local value names can either be simple constants or can be more complex expressions that transform or combine values from elsewhere in the module.
- 
-}  
 
+  # The expressions assigned to local value names can either be simple constants or can be more complex expressions that transform or combine values from elsewhere in the module.
+  
+# With Equals (==)
+  vnet_address_space = (var.environment == "dev" ? var.vnet_address_space_dev : var.vnet_address_space_all)
+
+# With Not Equals (!=)
+  #vnet_address_space = (var.environment != "dev" ? var.vnet_address_space_all : var.vnet_address_space_dev)
+}
 -----------------------------------------------------------------------------------------------------------------------------------------
 
 # Explanation: - 
 
-## Understanding the locals Block in Terraform
+## Understanding the local's Block in Terraform
 
 The locals block in Terraform defines reusable expressions that help simplify configurations, improve readability, and avoid repetition. 
 
