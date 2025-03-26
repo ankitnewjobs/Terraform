34210192c121b91d6c46dd45416012e6dd3b422b
Ankit Ranjan <154872782+ankitnewjobs@users.noreply.github.com>
2025-03-26 09:02:54 +0530

    Update c3-locals.tf

--- Day:- 39 Terraform-Remote-State-Datasource/project-2-app1/c3-locals.tf
+++ Day:- 39 Terraform-Remote-State-Datasource/project-2-app1/c3-locals.tf
@@ -1,14 +1,22 @@
 # Local Values Block
-locals {
+
+locals 
+{
   # Use-case-1: Shorten the names for more readability
   vm_name = "${var.business_unit}-${var.environment}-${var.virtual_machine_name}"
   
   # Use-case-2: Common tags to be assigned to all resources
+
   service_name = "Demo Services"
   owner = "Kalyan Reddy Daida"
-  common_tags = {
+  common_tags = 
+{
     Service = local.service_name
     Owner   = local.owner
     #Tag = "demo-tag1"
   }
 }
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Explanation: - 
