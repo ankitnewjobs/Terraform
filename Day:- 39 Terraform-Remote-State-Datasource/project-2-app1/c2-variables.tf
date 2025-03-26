398c17bf8922072dfa97bab02f4e71bccc6da259
Ankit Ranjan <154872782+ankitnewjobs@users.noreply.github.com>
2025-03-26 09:02:28 +0530

    Update c2-variables.tf

--- Day:- 39 Terraform-Remote-State-Datasource/project-2-app1/c2-variables.tf
+++ Day:- 39 Terraform-Remote-State-Datasource/project-2-app1/c2-variables.tf
@@ -1,23 +1,34 @@
 # Input Variables
 
 # 1. Business Unit Name
-variable "business_unit" {
+
+variable "business_unit"
+{
   description = "Business Unit Name"
   type = string
   default = "hr"
 }
+
 # 2. Environment Name
-variable "environment" {
+
+variable "environment" 
+{
   description = "Environment Name"
   type = string
   default = "dev"
 }
 
 # 3. Virtual Machine Name
-variable "virtual_machine_name" {
+
+variable "virtual_machine_name" 
+{
   description = "Virtual Machine Name"
   type = string 
 }
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Explanation: - 
 
 
 
