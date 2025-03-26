672a92281a74443be136911a395900314d747617
Ankit Ranjan <154872782+ankitnewjobs@users.noreply.github.com>
2025-03-26 09:01:53 +0530

    Update c0-terraform-remote-state-datasource.tf

--- Day:- 39 Terraform-Remote-State-Datasource/project-2-app1/c0-terraform-remote-state-datasource.tf
+++ Day:- 39 Terraform-Remote-State-Datasource/project-2-app1/c0-terraform-remote-state-datasource.tf
@@ -1,4 +1,5 @@
 # Terraform Remote State Datasource
+
 data "terraform_remote_state" "project1" {
   backend = "azurerm"
   config = {
@@ -10,10 +11,17 @@ data "terraform_remote_state" "project1" {
 }
 
 /* 
+
 1. Resource Group Name
 data.terraform_remote_state.project1.outputs.resource_group_name
+
 2. Resource Group Location
 data.terraform_remote_state.project1.outputs.resource_group_location
+
 3. Network Interface ID
 data.terraform_remote_state.project1.outputs.network_interface_id
 */
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Explanation: - 
