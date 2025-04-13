fb39b3631c89f538c7513213afe840d8cc226098
Ankit Ranjan <154872782+ankitnewjobs@users.noreply.github.com>
2025-04-13 21:28:07 +0530

    Update c7-outputs.tf

--- Day:- 42 Terraform-Workspaces-with-Local-Backends/terraform-manifests/c7-outputs.tf
+++ Day:- 42 Terraform-Workspaces-with-Local-Backends/terraform-manifests/c7-outputs.tf
@@ -30,4 +30,7 @@ output "vm_public_ip_address"
   value = azurerm_linux_virtual_machine.mylinuxvm.public_ip_address
 }
 
+-------------------------------------------------------------------------------------------------------------------------------------------
+
+# Explanation: - 
 
