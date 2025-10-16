c84bf1fa427b4d2913f8704f319c0b4d3d813eb9
Ankit Ranjan <154872782+ankitnewjobs@users.noreply.github.com>
2025-10-16 09:51:38 +0530

    Update versions.tf

--- Day:- 52 Terraform-Module-Publish-to-Public-Registry/terraform-azure-static-website-module-manifests/versions.tf
+++ Day:- 52 Terraform-Module-Publish-to-Public-Registry/terraform-azure-static-website-module-manifests/versions.tf
@@ -1,14 +1,23 @@
 # Terraform Block
-terraform {
+
+terraform 
+{
   required_version = ">= 1.0.0"
-  required_providers {
-    azurerm = {
+  required_providers 
+{
+    azurerm = 
+{
       source = "hashicorp/azurerm"
       version = ">= 2.0" 
     }  
-    random = {
+    random = 
+{
       source = "hashicorp/random"
       version = ">= 3.0"
     }           
   }
 }
+
+----------------------------------------------------------------------------------------------------------------------------------------
+
+# Explanation: - 
