d50c6d7956050d937367266fdcbd6b9bf898214d
Ankit Ranjan <154872782+ankitnewjobs@users.noreply.github.com>
2025-10-16 09:54:17 +0530

    Update c3-static-website.tf

--- Day:- 52 Terraform-Module-Publish-to-Public-Registry/terraform-manifests/c3-static-website.tf
+++ Day:- 52 Terraform-Module-Publish-to-Public-Registry/terraform-manifests/c3-static-website.tf
@@ -20,3 +20,7 @@ module "azure_static_website"
   static_website_index_document     = "index.html"
   static_website_error_404_document = "error.html"
 }
+
+----------------------------------------------------------------------------------------------------------------------------------------
+
+# Explanation: - 
