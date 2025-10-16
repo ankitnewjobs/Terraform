a34d9f3bb096103b90f3f22a17c932f71b994a25
Ankit Ranjan <154872782+ankitnewjobs@users.noreply.github.com>
2025-10-16 09:51:09 +0530

    Update variables.tf

--- Day:- 52 Terraform-Module-Publish-to-Public-Registry/terraform-azure-static-website-module-manifests/variables.tf
+++ Day:- 52 Terraform-Module-Publish-to-Public-Registry/terraform-azure-static-website-module-manifests/variables.tf
@@ -1,33 +1,46 @@
 # Input variable definitions
-variable "location" {
+
+variable "location" 
+{
   description = "The Azure Region in which all resource groups should be created."
   type = string 
 }
-variable "resource_group_name" {
+variable "resource_group_name" 
+{
   description = "The name of the resource group"
   type = string     
 }
-variable "storage_account_name" {
+variable "storage_account_name" 
+{
   description = "The name of the storage account"
   type = string   
 }
-variable "storage_account_tier" {
+variable "storage_account_tier"
+{
   description = "Storage Account Tier"
   type = string   
 }
-variable "storage_account_replication_type" {
+variable "storage_account_replication_type" 
+{
   description = "Storage Account Replication Type"
   type = string   
 }
-variable "storage_account_kind" {
+variable "storage_account_kind" 
+{
   description = "Storage Account Kind"
   type = string   
 }
-variable "static_website_index_document" {
+variable "static_website_index_document" 
+{
   description = "static website index document"
   type = string   
 }
-variable "static_website_error_404_document" {
+variable "static_website_error_404_document" 
+{
   description = "static website error 404 document"
   type = string   
 }
+
+----------------------------------------------------------------------------------------------------------------------------------------
+
+# Explanation: - 
